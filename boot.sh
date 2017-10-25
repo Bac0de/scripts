33f9bccb201efdeae498d5ccf16a31e41febce90
Park Ju Hyung <qkrwngud825@gmail.com>
2017-10-26 01:16:49 +0900

    boot: optimize kernel tunables
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>

--- boot.sh
+++ boot.sh
@@ -10,8 +10,31 @@ sleep 1
 # Start libvirtd
 service libvirtd start
 
-# Initialize libvirt network
+# Initialize libvirt default network
 virsh net-start default
 
 # Fix /dev/kvm node permission
 chown libvirt-qemu:libvirt-qemu /dev/kvm
+
+# Set hugepages
+set_hugepages.sh
+
+# Set tunables
+# - Disable THP
+#   QEMU runs on hugepages explicitly, so disable THP and save memory for system
+echo "Disabling THP"
+echo "never" > /sys/kernel/mm/transparent_hugepage/enabled
+echo "never" > /sys/kernel/mm/transparent_hugepage/defrag
+# - Disable entropy contributions
+#   Should be the default for non-rotational storage,
+#   disable it altogether to prevent additional latency
+echo "Disabling entropy contributions"
+find /sys -name add_random -exec sh -c 'echo "0" > {}' \;
+# - Set I/O schedulers to CFQ
+#   Distribute I/O requests to VMs fairly
+echo "Setting I/O schedulers to CFQ"
+find /sys/devices -name scheduler | grep -v virtual | while read file; do echo cfq > $file; done
+# - Set readahead to 128 kB
+#   Not meaningful to non-rotational storage, set to minimum to minimize memory thrashing
+echo "Setting readahead buffer to minimum (128kB)"
+find /sys/devices -name read_ahead_kb | grep -v virtual | while read file; do echo 128 > $file; done
