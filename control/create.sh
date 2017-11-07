bbf8dee69c0bf777a015306666a467d688bee5a0
Park Ju Hyung <qkrwngud825@gmail.com>
2017-11-07 23:20:39 +0900

    control: allow forking from another VM
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>

--- control/create.sh
+++ control/create.sh
@@ -2,14 +2,19 @@
 
 NAME=win10
 ID=$1
-# $2 in GB unit
+FROM=$2
+# $3 in GB unit
 # SIZE is only relevant when creating an initial image
 # TODO : allow multiple guests to have its own dynamic sizes
-SIZE=$2
+SIZE=$3
 
 UUID=$(uuidgen)
 # batu -> ba:40
 MAC=$(hexdump -n4 -e'/4 "ba:40" 4/1 ":%02x"' /dev/urandom)
+
+if [[ -z "$FROM" ]]; then
+  FROM=0
+fi
 
 echo "Creating $NAME-$ID"
 echo "UUID - $UUID"
@@ -22,22 +27,31 @@ sed -i -e s@BATU_CREATE_NAME@$NAME-$ID@g /tmp/$NAME-$ID-$UUID.xml
 sed -i -e s@BATU_CREATE_UUID@$UUID@g     /tmp/$NAME-$ID-$UUID.xml
 sed -i -e s@BATU_CREATE_MAC@$MAC@g       /tmp/$NAME-$ID-$UUID.xml
 
-# Fork base image
 cd /var/lib/libvirt/images
-if [ -e $NAME-common.qcow2 ]; then
-  qemu-img create -f qcow2 -b $NAME-common.qcow2 $NAME-$ID.qcow2
+if [[ "$FROM" != "0" ]]; then
+  # Fork from another VM
+  ORIG=$FROM-$(date "+%y%m%d")-$(uuidgen)
+  /etc/batu/control/initial.sh $FROM $ORIG
+  qemu-img create -f qcow2 -b $NAME-$ORIG.qcow2 $NAME-$ID.qcow2
   sed -i -e s@BATU_CREATE_IMG@$NAME-$ID.qcow2@g /tmp/$NAME-$ID-$UUID.xml
 else
-  # Create an initial image
-  # ID must be 1
-  if [[ "$ID" != "1" ]]; then
-    echo "Initial image hasn't been created with $NAME-1!" 1>&2
-    echo "Please run initial.sh after setting up $NAME-1!" 1>&2
-    exit 1
+  # Fork from base image
+  if [ -e $NAME-common.qcow2 ]; then
+    qemu-img create -f qcow2 -b $NAME-common.qcow2 $NAME-$ID.qcow2
+    sed -i -e s@BATU_CREATE_IMG@$NAME-$ID.qcow2@g /tmp/$NAME-$ID-$UUID.xml
+  else
+    # Create an initial image
+    # ID must be 1
+    if [[ "$ID" != "1" ]]; then
+      echo "Initial image hasn't been created with $NAME-1!" 1>&2
+      echo "Please run initial.sh after setting up $NAME-1!" 1>&2
+      exit 1
+    fi
+    qemu-img create -f qcow2 -S ${SIZE}G $NAME-$ID.qcow2
   fi
-  qemu-img create -f qcow2 -S ${SIZE}G $NAME-$ID.qcow2
-  chown libvirt-qemu:kvm $NAME-$ID.qcow2
 fi
 
+chown libvirt-qemu:kvm $NAME-$ID.qcow2
+virsh undefine $NAME-$ID 2>/dev/null
 virsh define /tmp/$NAME-$ID-$UUID.xml
 rm /tmp/$NAME-$ID-$UUID.xml
--- control/initial.sh
+++ control/initial.sh
@@ -1,21 +1,31 @@
 #!/bin/bash
 
 # Execute when initial VM(common) is setup
+# or creating a snapshot
+# TODO : implement removal(GC)
 
 NAME=win10
-ID=1
+ID=$1
+TO=$2
+
+if [[ -z "$ID" ]]; then
+  ID=1
+fi
+if [[ -z "$TO" ]]; then
+  TO=common
+fi
 
 cd /var/lib/libvirt/images
-echo "Moving $NAME-$ID.qcow2 to $NAME-common.qcow2"
-mv $NAME-$ID.qcow2 $NAME-common.qcow2
-touch $NAME-common_is_protected_via_chattr
-chown libvirt-qemu:kvm $NAME-common.qcow2
+echo "Moving $NAME-$ID.qcow2 to $NAME-$TO.qcow2"
+mv $NAME-$ID.qcow2 $NAME-$TO.qcow2
+touch $NAME-${TO}_is_protected_via_chattr
+chown libvirt-qemu:kvm $NAME-$TO.qcow2
 # Apply CAP_LINUX_IMMUTABLE
-chattr +i $NAME-common.qcow2
-echo "$NAME-common.qcow2 is now write-protected"
+chattr +i $NAME-$TO.qcow2
+echo "$NAME-$TO.qcow2 is now write-protected"
 
-echo "Creating $NAME-$ID.qcow2 from $NAME-common.qcow2"
-qemu-img create -f qcow2 -b $NAME-common.qcow2 $NAME-$ID.qcow2
+echo "Creating $NAME-$ID.qcow2 from $NAME-$TO.qcow2"
+qemu-img create -f qcow2 -b $NAME-$TO.qcow2 $NAME-$ID.qcow2
 chown libvirt-qemu:kvm $NAME-$ID.qcow2
 
 echo "Syncing..."
